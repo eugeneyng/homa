5c1bf360b3f638cc426a9e35d498a5dea4ef1089
eugene <ngyeugene@gmail.com>
2022-05-04 04:25:37 +0000

    misunderstanding of react - should change all class components to function components

--- src/index.js
+++ src/index.js
@@ -5,30 +5,24 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 import * as Pages from "./pages"
 import * as Auth from "./utilities"
 
-class Homa extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-    };
-  }
+function Homa () {
 
   const [token, setToken] = React.useState();
-
-  render() {
-    return (
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Pages.Home />} />
-          <Route path="places" element={<Pages.Places />} >
-            <Route path=":placeId" element={<Pages.Place />} />
-          </Route>
-          <Route path="login" element={<Pages.Login />} />
-          <Route path="*" element={<Pages.NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    )
-  }
+  
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Pages.Home />} />
+        <Route path="places" element={<Pages.Places />} >
+          <Route path=":placeId" element={<Pages.Place />} />
+        </Route>
+        <Route path="login" element={<Pages.Login />} />
+        <Route path="*" element={<Pages.NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
+
 
 const root = ReactDOMClient.createRoot(document.getElementById("root"));
 root.render(<Homa />);
